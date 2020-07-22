29a73ea10b3d31c42318b180e665b823167f0757
Azune-NK <w1ndy@inbox.ru>
2020-07-22 17:29:45 +0300

    фиксит модалки + базовые стили страницы компонентов

--- source/js/modules/init-modals.js
+++ source/js/modules/init-modals.js
@@ -1,5 +1,6 @@
 import {setupModal} from '../utils/modal';
 
+const modals = document.querySelectorAll('.modal');
 const modalFeedback = document.querySelector('.modal--feedback');
 const modalFeedbackBtn = document.querySelectorAll('[data-modal="feedback"]');
 const modalSuccess = document.querySelector('.modal--success');
@@ -9,6 +10,17 @@ const modalSuccessBtns = document.querySelectorAll('[data-modal="success"]');
 // возможна инициализация только с первыми аргументом,
 // если вам нужно открывать модалку в другом месте под какими-нибудь условиями
 const initModals = () => {
+  // фикс для редких случаев, когда модалка появляется при загрузке страницы
+  window.onload = () => {
+    if (modals.length) {
+      modals.forEach((el) => {
+        setTimeout(() => {
+          el.classList.remove('modal--preload');
+        }, 100);
+      });
+    }
+  };
+
   if (modalFeedback && modalFeedbackBtn.length) {
     setupModal(modalFeedback, false, modalFeedbackBtn, false, false);
   }
