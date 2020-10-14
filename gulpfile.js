fa0524b53b81b0de7fa01f028b780c512f536397
Azune-NK <w1ndy@inbox.ru>
2020-10-14 10:11:53 +0300

    обновляет галп до экспортов и добавляет webpack env dev\prod

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,56 @@
-const gulp = require(`gulp`);
-const plumber = require(`gulp-plumber`);
-const sourcemap = require(`gulp-sourcemaps`);
-const sass = require(`gulp-sass`);
-const postcss = require(`gulp-postcss`);
-const autoprefixer = require(`autoprefixer`);
-const server = require(`browser-sync`).create();
-const csso = require(`gulp-csso`);
-const rename = require(`gulp-rename`);
-const imagemin = require(`gulp-imagemin`);
-const webp = require(`gulp-webp`);
-const svgstore = require(`gulp-svgstore`);
-const del = require(`del`);
-const uglify = require(`gulp-uglify`);
-const webpackStream = require(`webpack-stream`);
-const webpackConfig = require(`./webpack.config.js`);
-const concat = require(`gulp-concat`);
-const fileinclude = require(`gulp-file-include`);
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const sourcemap = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const server = require('browser-sync').create();
+const csso = require('gulp-csso');
+const rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const svgstore = require('gulp-svgstore');
+const del = require('del');
+const webpackStream = require('webpack-stream');
+const webpackConfig = require('./webpack.config.js');
+const fileinclude = require('gulp-file-include');
 
-gulp.task(`html`, function () {
-  return gulp.src([`source/html/*.html`])
+const html = () => {
+  return gulp.src(['source/html/*.html'])
     .pipe(fileinclude({
-      prefix: `@@`,
-      basepath: `@root`,
+      prefix: '@@',
+      basepath: '@root',
       context: { // глобальные переменные для include
-        test: `text`
+        test: 'text'
       }
     }))
-    .pipe(gulp.dest(`build`));
-});
+    .pipe(gulp.dest('build'));
+};
 
-gulp.task(`css`, function () {
-  return gulp.src(`source/sass/style.scss`)
+const css = () => {
+  return gulp.src('source/sass/style.scss')
       .pipe(plumber())
       .pipe(sourcemap.init())
       .pipe(sass())
       .pipe(postcss([autoprefixer({
         grid: true,
       })]))
-      .pipe(gulp.dest(`build/css`))
+      .pipe(gulp.dest('build/css'))
       .pipe(csso())
-      .pipe(rename(`style.min.css`))
-      .pipe(sourcemap.write(`.`))
-      .pipe(gulp.dest(`build/css`))
+      .pipe(rename('style.min.css'))
+      .pipe(sourcemap.write('.'))
+      .pipe(gulp.dest('build/css'))
       .pipe(server.stream());
-});
+};
 
-gulp.task(`script`, function () {
-  return gulp.src([`source/js/main.js`])
+const js = () => {
+  return gulp.src(['source/js/main.js'])
       .pipe(webpackStream(webpackConfig))
-      .pipe(uglify())
-      .pipe(gulp.dest(`build/js`));
-});
+      .pipe(gulp.dest('build/js'))
+};
 
-gulp.task(`svgo`, function () {
-  return gulp.src(`source/img/**/*.{svg}`)
+const svgo = () => {
+  return gulp.src('source/img/**/*.{svg}')
       .pipe(imagemin([
         imagemin.svgo({
             plugins: [
@@ -63,99 +60,92 @@ gulp.task(`svgo`, function () {
             ]
           }),
       ]))
-      .pipe(gulp.dest(`source/img`));
-});
+      .pipe(gulp.dest('source/img'));
+};
 
-gulp.task(`sprite`, function () {
-  return gulp.src(`source/img/sprite/*.svg`)
+const sprite = () => {
+  return gulp.src('source/img/sprite/*.svg')
       .pipe(svgstore({inlineSvg: true}))
-      .pipe(rename(`sprite_auto.svg`))
-      .pipe(gulp.dest(`build/img`));
-});
+      .pipe(rename('sprite_auto.svg'))
+      .pipe(gulp.dest('build/img'));
+};
 
-gulp.task(`server`, function () {
+const syncserver = () => {
   server.init({
-    server: `build/`,
+    server: 'build/',
     notify: false,
     open: true,
     cors: true,
     ui: false,
   });
 
-  gulp.watch(`source/html/**/*.html`, gulp.series(`html`, `refresh`));
-  gulp.watch(`source/sass/**/*.{scss,sass}`, gulp.series(`css`));
-  gulp.watch(`source/js/**/*.js`, gulp.series(`script`, `refresh`));
-  gulp.watch(`source/img/**/*.svg`, gulp.series(`copysvg`, `sprite`, `html`, `refresh`));
-  gulp.watch(`source/img/**/*.{png,jpg}`, gulp.series(`copypngjpg`, `html`, `refresh`));
-});
+  gulp.watch('source/html/**/*.html', gulp.series(html, refresh));
+  gulp.watch('source/sass/**/*.{scss,sass}', gulp.series(css));
+  gulp.watch('source/js/**/*.{js,json}', gulp.series(js, refresh));
+  gulp.watch('source/data/**/*.{js,json}', gulp.series(copy, refresh));
+  gulp.watch('source/img/**/*.svg', gulp.series(copysvg, sprite, html, refresh));
+  gulp.watch('source/img/**/*.{png,jpg}', gulp.series(copypngjpg, html, refresh));
+};
 
-gulp.task(`refresh`, function (done) {
+const refresh = (done) => {
   server.reload();
   done();
-});
+};
 
-gulp.task(`copysvg`, function () {
-  return gulp.src(`source/img/**/*.svg`, {base: `source`})
-      .pipe(gulp.dest(`build`));
-});
+const copysvg = () => {
+  return gulp.src('source/img/**/*.svg', {base: 'source'})
+      .pipe(gulp.dest('build'));
+};
 
-gulp.task(`copypngjpg`, function () {
-  return gulp.src(`source/img/**/*.{png,jpg}`, {base: `source`})
-      .pipe(gulp.dest(`build`));
-});
+const copypngjpg = () => {
+  return gulp.src('source/img/**/*.{png,jpg}', {base: 'source'})
+      .pipe(gulp.dest('build'));
+};
 
-gulp.task(`copy`, function () {
+const copy = () => {
   return gulp.src([
-    `source/fonts/**/*.{woff,woff2}`,
-    `source/favicon/**`,
-    `source/img/**`,
-    `source/*.php`,
-    `source/video/**`, // учтите, что иногда git искажает видеофайлы, pdf и gif - проверяйте и если обнаруживаете баги - скидывайте тестировщику такие файлы напрямую
-    `source/downloads/**`,
+    'source/fonts/**',
+    'source/favicon/**',
+    'source/img/**',
+    'source/data/**',
+    'source/file/**',
+    'source/*.php',
+    'source/video/**', // учтите, что иногда git искажает видеофайлы, некоторые шрифты, pdf и gif - проверяйте и если обнаруживаете баги - скидывайте тестировщику такие файлы напрямую
+    'source/downloads/**',
   ], {
-    base: `source`,
+    base: 'source',
   })
-      .pipe(gulp.dest(`build`));
-});
+      .pipe(gulp.dest('build'));
+};
 
-gulp.task(`clean`, function () {
-  return del(`build`);
-});
+const clean = () => {
+  return del('build');
+};
 
-gulp.task(`build`, gulp.series(
-    `clean`,
-    `svgo`,
-    `copy`,
-    `sprite`,
-    `css`,
-    `script`,
-    `html`
-));
+const build = gulp.series(clean, svgo, copy, css, sprite, js, html);
 
-gulp.task(`start`, gulp.series(`build`, `server`));
+const start = gulp.series(build, syncserver);
 
 // Optional tasks
 //---------------------------------
-// Вызывайте через `npm run taskName`
+// Вызывайте через 'npm run taskName'
 
-gulp.task(`webp`, function () {
-  return gulp.src(`source/img/**/*.{png,jpg}`)
+const createWebp = () => {
+  return gulp.src('source/img/**/*.{png,jpg}')
       .pipe(webp({quality: 90}))
-      .pipe(gulp.dest(`source/img`));
-});
+      .pipe(gulp.dest('source/img'));
+};
 
-gulp.task(`imagemin`, function () {
-  return gulp.src(`build/img/**/*.{png,jpg}`)
+const optimizeImages = () => {
+  return gulp.src('build/img/**/*.{png,jpg}')
       .pipe(imagemin([
         imagemin.optipng({optimizationLevel: 3}),
         imagemin.mozjpeg({quality: 75, progressive: true}),
       ]))
-      .pipe(gulp.dest(`build/img`));
-});
+      .pipe(gulp.dest('build/img'));
+};
 
-// для отправки заказчику неминифицированного js "для чтения"
-gulp.task(`concat-js`, function () {
-  return gulp.src([`source/js/main.js`, `source/js/utils/**/*.js`, `source/js/modules/**/*.js`])
-    .pipe(concat(`main.readonly.js`))
-    .pipe(gulp.dest(`build/js`));
-});
+exports.build = build;
+exports.start = start;
+exports.webp = createWebp;
+exports.imagemin = optimizeImages;
