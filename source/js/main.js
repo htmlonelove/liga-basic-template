a26d03e3505400d69d19151b0dbcee00f0fab0c4
Лига А <53946851+htmlonelove@users.noreply.github.com>
2021-08-16 14:21:30 +0300

    Update main.js

--- source/js/main.js
+++ source/js/main.js
@@ -3,10 +3,6 @@ import {iosVhFix} from './utils/ios-vh-fix';
 
 import {initModals} from './modules/init-modals';
 
-//-- этот скрипт загружает модуль для тестирования, его не должно быть в финальном билде и на проде для клиента
-import {testInstruments} from './vendor/testInstruments';
-//--
-
 // Utils
 // ---------------------------------
 
@@ -17,9 +13,3 @@ iosVhFix();
 // ---------------------------------
 
 initModals();
-
-//-- этот скрипт загружает модуль для тестирования, его не должно быть в финальном билде и на проде для клиента
-window.addEventListener('load', () => {
-  testInstruments();
-});
-//--
